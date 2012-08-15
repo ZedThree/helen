4e16f86de9e1fd70a114472c36c0b024b988b15c
Peter Hill <zed.three@gmail.com>
2012-08-15 17:34:15 +0100

    Adds the ability to generate all the namelists from a source file. The namelists contain InputParam objects.

--- launcher.py
+++ launcher.py
@@ -1,41 +1,53 @@
 from enthought.traits.api import *
 from enthought.traits.ui.api import *
+from xml.etree.ElementTree import Element, SubElement, Comment
+from ElementTree_pretty import prettify
 
 import re
+import copy
 
 readinputfile='/home/peter/nemorb/peter/src/readinput.F90'
 globalfile='/home/peter/nemorb/peter/src/globals.F90'
 
-class input_param(HasTraits):
+class InputParam(HasTraits):
     """ An input parameter """
+    
+    view = View(Item(name = 'value'),
+                buttons = [OKButton, CancelButton])
 
     def __init__(self, param_name, species=None):
         self.type = get_type(globalfile, param_name)
+        # N.B. Need to implement a different get_default for parameters
+        #      in 'attributes' namelist - also for electrons!
         self.default = get_default(globalfile, param_name)
         self.description = get_desc(readinputfile, param_name)
         # if species is not None:
         #     # This will be important later
         #     # This means we'll want to check readinputfile, not globalfile
         if self.type is 'float':
-            self.value = real_param(name=param_name, default=self.default,
-                                    desc_in=self.description)
+            real_param(self, name=param_name, default=self.default,
+                       param_desc=self.description)
         if self.type is 'int':
-            self.value = int_param(name=param_name, default=self.default,
-                                    desc_in=self.description)
+            int_param(self, name=param_name, default=self.default,
+                      param_desc=self.description)
         if self.type is 'str':
-            self.value = str_param(name=param_name, default=self.default,
-                                    desc_in=self.description)
+            str_param(self, name=param_name, default=self.default,
+                      param_desc=self.description)
         if self.type is 'bool':
-            self.value = bool_param(name=param_name, default=self.default,
-                                    desc_in=self.description)
+            bool_param(self, name=param_name, default=self.default,
+                       param_desc=self.description)
             
 
-
-# class namelist(dict):
-#     """ A Fortran namelist """
+class Namelist(dict):
+    """ 
+    A Fortran Namelist object.
+    A dictionary of InputParam objects.
+    """
+    # This should hold specific values of input parameters,
+    # either from having been read in, or from being created
     
-#     def __init_(self):
-        
+    def __init_(self, name):
+        self.name = name
     
     
 class InputFile(dict):
@@ -51,71 +63,62 @@ class InputFile(dict):
 
     
 
-class real_param(HasTraits):
-    """ A Traits object for a Fortran real parameter """
-   
-    def __init__(self, name='Value', default=None, desc_in=''):
-        HasTraits.__init__(self)
-        if default is None:
-            default = 0.0
-        self.add_trait('value',Float(default,label=name,desc=desc_in))
+def real_param(param_object, name, param_desc, default=None):
+    """ Adds a Traits object for a Fortran real parameter """
+    if default is None:
+        default = 0.0
+    param_object.add_trait('value',Float(default,label=name,desc=param_desc))
 
-class int_param(HasTraits):
+def int_param(param_object, name, param_desc, default=None):
     """ A Traits object for a Fortran integer parameter """
-   
-    def __init__(self, name='Value', default=None, desc_in=''):
-        if default is None:
-            default = 0
-        self.add_trait('value',CInt(default,label=name,desc=desc_in))
+    if default is None:
+        default = 0
+    param_object.add_trait('value',CInt(default,label=name,desc=param_desc))
 
-class str_param(HasTraits):
+def str_param(param_object, name, param_desc, default=None):
     """ A Traits object for a Fortran character parameter """
-   
-    def __init__(self, name='Value', default=None, desc_in=''):
-        if default is None:
-            default = ''
-        self.add_trait('value',String(default,label=name,desc=desc_in))
+    if default is None:
+        default = ''
+    param_object.add_trait('value',String(default,label=name,desc=param_desc))
 
-class bool_param(HasTraits):
+def bool_param(param_object, name, param_desc, default=None):
     """ A Traits object for a Fortran logical parameter """
+    if default is None:
+        default = False
+    param_object.add_trait('value',Bool(default,label=name,desc=param_desc))
+
+class scan_param(HasTraits):
+    """ Choose which parameter to scan over """
    
-    def __init__(self, name='Value', default=None, desc_in=''):
-        if default is None:
-            default = False
-        self.add_trait('value',Bool(default,label=name,desc=desc_in))
-
-class all_params(HasTraits):
-    """ All the possible input parameters """
-   
-    def __init__(self, inputlist=''):
-        self.add_trait('obj',Enum(inputlist,
-                                  label="Parameter",
-                                  desc="Input parameter to scan over",))
-
-
+    def __init__(self, namelist_in):
+        paramlist = sorted(set(flatten(namelist_in)))
+        self.add_trait('scan_param',Enum(paramlist,
+                                        label="Parameter",
+                                        desc="Input parameter to scan over",))
 
 
 def pull_namelists(infile):
     """ Pull namelists out of file """
     
     # Read file
-    f = open(infile,'r').read()
-    # Collapse multilines
-    f = re.sub("&[\s\t]*\n","",f)
+    with open(infile,'r') as f:
+        f_content = f.read()
+        # Collapse multilines
+        f_content = re.sub("&[\s\t]*\n","",f_content)
 
-    # Make regexp pattern
-    nml_pattern = re.compile(r"NAMELIST[\s\t]*/(\w+)/[a-zA-Z0-9_,\s\t]*\n")
-    # Find all the matches in the file for pattern, return as iterable object
-    iterator = nml_pattern.finditer(f)
+        # Make regexp pattern
+        nml_pattern = re.compile(r"NAMELIST[\s\t]*/(\w+)/[a-zA-Z0-9_,\s\t]*\n")
+        # Find all the matches in the file for pattern, return as iterable object
+        iterator = nml_pattern.finditer(f_content)
     
-    # Make a namelist array
-    nml = []
-    # Populate the array with the matches from the file
-    for match in iterator:
-        tmp = match.group()
-        nml.append(tmp)
+        # Make a namelist array
+        nml = []
+        # Populate the array with the matches from the file
+        for match in iterator:
+            tmp = match.group()
+            nml.append(tmp)
 
-        
+            
     # Clean up namelist - remove multi-spaces, commas, and slashes
     nml = [re.sub("[\s\t]+"," ",item).strip() for item in nml]
     nml = [re.sub("[/,]"," ",item).strip() for item in nml]
@@ -132,7 +135,7 @@ def get_type(infile, param):
     """ Determine a parameter's type by searching infile """
     
     # Pattern matching a definition line
-    def_pattern = re.compile(r".*::[\s\ta-zA-Z_=0-9]*"+param)
+    def_pattern = re.compile(r".*::[\s\ta-zA-Z_=0-9,+-]*"+param,re.I)
     # Patterns for different types
     float_pat = re.compile("REAL")
     int_pat = re.compile("INTEGER")
@@ -155,8 +158,8 @@ def get_type(infile, param):
             param_type = 'str'
         elif bool_pat.search(def_line.group()):
             param_type = 'bool'
-        else:
-            param_type = 'err'
+    else:
+        param_type = 'err'
         
     return param_type
 
@@ -166,7 +169,7 @@ def get_desc(infile, param):
     # There are descriptions of each parameter in globals.F90, where they are defined
     # There are better descriptions in readinput.F90, where they are stored!
     # A couple of records might be stupid...
-    desc_pattern = re.compile(r"parameter.*\'" + param + "\',\'([a-zA-Z\s_]+)\'")
+    desc_pattern = re.compile(r"parameter.*\'" + param + "\',\'([^\']+)\'")
     desc_pattern2 = re.compile(r"creatd.*/" + param + "\',\'([a-zA-Z\s_]+)\'")
 
     # Open the file, read it, then close
@@ -177,6 +180,7 @@ def get_desc(infile, param):
     # Search file for store_parameter
     desc_line = desc_pattern.search(f_content)
     desc_line2 = desc_pattern2.search(f_content)
+    param_desc = ''
     if desc_line:
         param_desc = desc_line.group(1)
     elif desc_line2:
@@ -204,6 +208,93 @@ def get_default(infile, param):
 
     return param_default
     
+def flatten(d):
+    """Recursively flatten dictionary values in `d`.
+
+    >>> hat = {'cat': ['images/cat-in-the-hat.png'],
+    ...        'fish': {'colours': {'red': [0xFF0000], 'blue': [0x0000FF]},
+    ...                 'numbers': {'one': [1], 'two': [2]}},
+    ...        'food': {'eggs': {'green': [0x00FF00]},
+    ...                 'ham': ['lean', 'medium', 'fat']}}
+    >>> set_of_values = set(flatten(hat))
+    >>> sorted(set_of_values)
+    [1, 2, 255, 65280, 16711680, 'fat', 'images/cat-in-the-hat.png', 'lean', 'medium']
+    """
+    try:
+        for v in d.itervalues():
+            for nested_v in flatten(v):
+                yield nested_v
+    except AttributeError:
+        for list_v in d:
+            yield list_v
+
+def generate_namelist_from_source():
+    """
+    This will generate a set of namelists from the source code.
+    It should then write the namelist objects to file.
+    """
+
+    # The dictionary full of namelists and input parameters
+    # Structure is {NAMELIST1: [var1 ... varN] ...}
+    nml_dict = pull_namelists(readinputfile)
+    
+    # Now we want to create a second dictionary:
+    # this one will be nested with the structure
+    # {NAMELIST1: {var1: [InputParam object] ...} ...}
+    # The InputParam objects for the generated namelists 
+    # will contain the default values as traits.
+    full_dict = {}
+    for key in nml_dict.iterkeys():
+        full_dict[key] = namelist(name=key)
+        for item in nml_dict[key]:
+            full_dict[key][item] = InputParam(item)
+        
+    return full_dict
+
+
+def read_generated_namelist():
+    """
+    This will read a set of previously generated namelists from a file.
+    """
+
+def write_generated_namelist(nml_file=None, nml_dict=None):
+    """
+    This will write a set of previously generated namelists to a file
+    """
+    
+    if not nml_file:
+        # No file specified
+        return None
+    elif not nml_dict:
+        # No namelist specified
+        return None
+    else:
+        # Open file
+        fil=file(nml_file,'w')
+        
+        # Use a try, finally block to ensure file is always closed
+        try:
+            # Write file
+            fil.write('""" Some header block """')
+        finally:
+            fil.close()
+
+    return fil
+
+
+def read_inputfile():
+    """
+    This will read a set of namelists from an inputfile
+    """
+    # Structure of dictionary should be something like:
+    # inputfile = {0:{'BASIC': ...}, ...}
+    # with the different namelists being copy.deepcopy() of the namelist objects!
+    # Then we can have multiple copies of the same namelist.
+
+def write_inputfile():
+    """
+    This will write a set of namelists comprising an inputfile.
+    """
 
 if __name__ == "__main__":
     inputfile = InputFile()
